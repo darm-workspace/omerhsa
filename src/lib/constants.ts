f25a3d15dcc624b42e517d61eeede516a7abcf6f
darm-workspace <dav.retreage@gmail.com>
2026-03-11 13:13:38 -0600

    CReate project

---
+++ src/lib/constants.ts
@@ -0,0 +1,28 @@
+import logoOmerhsa from '../assets/images/logos/omerhsa.png';
+
+export const SITE_CONFIG = {
+  name: 'OMERHSA',
+  description: 'Correduría de Seguros experta en protección patrimonial.',
+  logo: logoOmerhsa,
+  contact: {
+    phone: '+504 2222-0000',
+    email: 'info@omerhsa.com',
+    address: 'Tegucigalpa, Honduras'
+  },
+  social: {
+    facebook: 'https://facebook.com',
+    instagram: 'https://instagram.com',
+    linkedin: 'https://linkedin.com',
+    tiktok: 'https://tiktok.com'
+  }
+};
+
+export const NAV_LINKS = [
+  { name: 'Inicio', href: '/' },
+  { name: 'Nosotros', href: '/nosotros' },
+  { name: 'Servicios', href: '/servicios' },
+  { name: 'Seguros', href: '/seguros' },
+  { name: 'Reclamos', href: '/reclamos' },
+  { name: 'Blog', href: '/blog' },
+  { name: 'Contacto', href: '/contacto' },
+];
