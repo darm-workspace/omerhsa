2445ad5832a4b274eca11f7eb0f38c48173de6b3
darm-workspace <dav.retreage@gmail.com>
2026-03-18 14:54:50 +0000

    Vercel Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,27 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import vue from '@astrojs/vue';
 import svelte from '@astrojs/svelte';
-import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-
+  site: 'https://omerhsa.com',
+  output: 'static',
+  
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      cssCodeSplit: true,
+    }
   },
 
-  integrations: [react(), vue(), svelte()],
+  integrations: [
+    react(),
+    vue(),
+    svelte(),
+    sitemap(),
+  ],
+
+  image: {
+    domains: ['randomuser.me'],
+  },
 });
